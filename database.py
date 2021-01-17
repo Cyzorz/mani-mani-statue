3a512749fac5c26041342439b1b512d6c00b84b8
Cyzorz <39070767+Cyzorz@users.noreply.github.com>
2021-01-17 14:17:28 -0800

    fix db issues
    hi

--- database.py
+++ database.py
@@ -1,47 +1,49 @@
 import sqlite3
 import os
 
+#database class
 class DB:
-    file = "../database.db"
+    file = "database.db"
 
+    #initalization
     def __init__(self):
         self.conn = sqlite3.connect(os.path.join(os.path.dirname(os.path.abspath(__file__)), self.file))
-        self.conn.row_factory = sqlite3.Row
         self.cursor = self.conn.cursor()
-        self.create_structure()
+        self.createStructure()
 
+    #util functions
     def execute(self, query, args = ()):
         try:
             self.cursor.execute(query, args)
             self.conn.commit()
             return True
-        except Exception as x:
-            print(x)
+        except Exception as e:
+            print(e)
             return False
-
     def fetch(self, query, args = ()):
         try:
             return self.cursor.execute(query, args).fetchall()
-        except Exception as x:
-            print(x)
+        except Exception as e:
+            print(e)
             return False
 
-    def create_structure(self):
-        self.execute("""CREATE TABLE IF NOT EXISTS points_table (
-            user_id INTEGER NOT NULL,
-            points INTEGER NOT NULL,
-            PRIMARY KEY (user_id));""")
+    #generates the db structure if not present
+    def createStructure(self):
+        self.execute("""CREATE TABLE IF NOT EXISTS points_table(
+            property VARCHAR(128) NOT NULL,
+            value TEXT NOT NULL,
+            PRIMARY KEY (property));""")
     
-    def find_points(self, user_id, default = False):
-        product = self.fetch("SELECT points FROM points_table WHERE user_id = ?", (user_id,))
+    def find_points(self, property, default = False):
+        product = self.fetch("SELECT value FROM points_table WHERE property = ?", (property,))
         if product != False and len(product) > 0:
-            return product[0]["points"]
+            return product[0][0]
         else:
             return default
 
-    def update_points(self, user_id, points):
-        self.execute("UPDATE points_table SET points = ? WHERE user_id = ?", (points, user_id))
+    def update_points(self, property, value):
+        self.execute("UPDATE points_table SET value = ? WHERE property = ?", (property, value))
         if self.cursor.rowcount == 0:
-            return self.execute("INSERT INTO points_table (user_id, points) VALUES (?, ?)", (user_id, points))
+            return self.execute("INSERT INTO points_table (property, value) VALUES (?, ?)", (property, value))
         else:
             return True
--- primary.py
+++ primary.py
@@ -4,6 +4,9 @@ from discord.ext import commands
 import qrcodegen as qr
 import config
 
+class Bot(discord.Client):
+    database = DB()
+
 client = commands.Bot(command_prefix="m!")
 
 async def on_ready(self):
@@ -13,13 +16,11 @@ async def on_ready(self):
 @client.command()
 async def qrcode(message):
     qr.qrGen.create()
-    #result = database.thing.find_points()
-    await message.channel.send(file = discord.File('qrcode_test.png'))
-    #if i == False:
-    #    i = 0
-    #    return True
-    DB.update_points(1, message.author.id)
-    #await message.channel.send(f"{message.author.mention} has {i} points!")
+    i = Bot.database.find_points(message.author.id)
+    if i == False:
+        i = 0
+    await message.channel.send(f"Current balance: {i}", file = discord.File('qrcode_test.png'))
+    Bot.database.update_points(int(message.author.id), int(i) + 1)
 
 client.run(config.TOKEN)
 
