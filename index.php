27c5352d1cdc6eabd1fa4b1707960a9d96666099
Cyzorz <root@vps.server.local>
2021-01-18 13:15:28 -0800

    Redirect. grab client ID from OAuth

--- index.php
+++ index.php
@@ -1,4 +1,62 @@
-include 'oauth.php';
-<html>
-    <meta http-equiv="Refresh" content="0; url='YOUR_REDIRECT_URL" />
-</html>
+
+<?php
+
+$tokenURL = 'https://discord.com/api/oauth2/token';
+$apiURLBase = 'https://discord.com/api/users/@me';
+
+session_start();
+
+if(get('code')) {
+
+  $token = apiRequest($tokenURL, array(
+    "grant_type" => "authorization_code",
+    'client_id' => YOUR_CLIENT_ID,
+    'client_secret' => "YOUR_CLIENT_SECRET",
+    'redirect_uri' => 'YOUR_REDIRECT_URI',
+    'code' => get('code')
+  ));
+  $_SESSION['access_token'] = $token->access_token;
+
+
+  header('Location: ' . $_SERVER['PHP_SELF']);
+}
+
+if(session('access_token')) {
+  $user = apiRequest($apiURLBase);
+  echo $user->id;
+
+} else {
+    header("Location: YOUR_REDIRECT_URL");
+}
+
+function apiRequest($url, $post=FALSE, $headers=array()) {
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+  $response = curl_exec($ch);
+
+
+  if($post)
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+
+  $headers[] = 'Accept: application/json';
+
+  if(session('access_token'))
+    $headers[] = 'Authorization: Bearer ' . session('access_token');
+
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+  $response = curl_exec($ch);
+  return json_decode($response);
+}
+
+function get($key, $default=NULL) {
+  return array_key_exists($key, $_GET) ? $_GET[$key] : $default;
+}
+
+function session($key, $default=NULL) {
+  return array_key_exists($key, $_SESSION) ? $_SESSION[$key] : $default;
+}
+
+?>
