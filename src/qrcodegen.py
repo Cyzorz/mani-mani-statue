1a3bc04eb4f91081f8ef06fa1cba28048319f456
Cyzorz <39070767+Cyzorz@users.noreply.github.com>
2021-02-09 11:32:00 -0800

    Update qrcodegen.py

--- src/qrcodegen.py
+++ src/qrcodegen.py
@@ -1,7 +1,8 @@
 import qrcode
+from path import Path
 import io
 from pyqrcode import QRCode
 class qrGen:
     def create():
         img = qrcode.make('http://pogchampion.atwebpages.com/')
-        img.save('assets/qrcode_test.png')
+        img.save(Path.get('assets/qrcode_test.png'))
