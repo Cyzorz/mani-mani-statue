45a0b9bb4cbf33e0ce871036b59d606b26629800
Cyzorz <39070767+Cyzorz@users.noreply.github.com>
2021-02-09 09:46:13 -0800

    Store QR code in embed

--- src/primary.py
+++ src/primary.py
@@ -16,10 +16,13 @@ async def on_ready(self):
 @client.command()
 async def qrcode(message):
     qr.qrGen.create()
+    file = discord.File("assets/qrcode_test.png", filename="qrcode_test.png")
+    embed= discord.Embed(title="QR Code", description = "Scan this to get a free token!", color = 0xffffff)
+    embed.set_image(url = "attachment://qrcode_test.png")
     i = Bot.database.find_points(message.author.id)
     if i == False:
         i = 0
-    await message.channel.send(f"Current balance: {i}", file = discord.File('assets/qrcode_test.png'))
+    await message.channel.send(file=file, embed=embed)
     Bot.database.update_points(int(message.author.id), int(i) + 1)
 
 client.run(config.TOKEN)
